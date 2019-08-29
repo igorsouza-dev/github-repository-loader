a924d8d6f74e7780d8c522a921b7207ebcb63b00
Igor Souza <igorsouza.dev@gmail.com>
2019-08-29 16:54:14 -0300

    Added repository details

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
+
+import { Form, SubmitButton, List } from './styles';
 
 class Main extends Component {
-  state = {
-    newRepo: '',
-    repositories: [],
-    loading: false,
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+    };
+  }
 
   componentDidMount() {
     const repositories = localStorage.getItem('repositories');
@@ -77,7 +84,9 @@ class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
