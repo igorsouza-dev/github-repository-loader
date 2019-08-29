5053a34ed2c25fa806f82b8ff2a9ef556f627f75
Igor Souza <igorsouza.dev@gmail.com>
2019-08-29 14:55:08 -0300

    Adding main page funcionality

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositories
-      </h1>
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Add repository" />
-        <SubmitButton disabled>
-          <FaPlus color="#fff" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+class Main extends Component {
+  state = {
+    newRepo: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.newRepo);
+  };
+
+  render() {
+    const { newRepo } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositories
+        </h1>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add repository"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton>
+            <FaPlus color="#fff" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default Main;
