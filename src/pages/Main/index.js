84d796865db62bff86d6d84e57468f5acef711c5
Igor Souza <igorsouza.dev@gmail.com>
2019-08-29 15:23:44 -0300

    Added github integration for repository searching. Added axios.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
 class Main extends Component {
   state = {
     newRepo: '',
+    repositories: [],
+    loading: false,
   };
 
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   };
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
-    console.log(this.state.newRepo);
+    this.setState({ loading: true });
+    const { newRepo, repositories } = this.state;
+    try {
+      const response = await api.get(`/repos/${newRepo}`);
+      const data = {
+        name: response.data.full_name,
+      };
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        loading: false,
+      });
+    } catch (error) {
+      alert('Repository doesnt exists');
+      this.setState({ loading: false });
+    }
   };
 
   render() {
-    const { newRepo } = this.state;
+    const { newRepo, loading } = this.state;
     return (
       <Container>
         <h1>
@@ -33,8 +51,12 @@ class Main extends Component {
             value={newRepo}
             onChange={this.handleInputChange}
           />
-          <SubmitButton>
-            <FaPlus color="#fff" size={14} />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
           </SubmitButton>
         </Form>
       </Container>
